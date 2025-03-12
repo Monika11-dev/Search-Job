c2dfd095416d1bfafda8083a296b4f3d5bd39de6
Monika11-dev <monika.chauhan@celestialsys.com>
2025-03-12 09:54:57 +0530

    theme modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/styles";
+import theme from "./Theme/theme";
 import { Layout } from "./Pages/Layout/Layout";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import LandingPage from "./Pages/Landing Page/LandingPage";
@@ -11,6 +13,7 @@ function App() {
   
   return (
     <>
+      <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Dashboard />} />
@@ -20,6 +23,7 @@ function App() {
           </Route>
           <Route path="/Login" element={<LandingPage />} />
         </Routes>
+      </ThemeProvider>
     </>
   );
 }
